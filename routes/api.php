0669d1d39ce5bc3b8d2017aa2efe5b093cf2a625
Victor Durand <vic20016@gmail.com>
2020-04-24 15:32:15 +0200

    fix: get method

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::group(['middleware' => 'jwt.verify'], function () {
     Route::group(['prefix' => 'booking'], function(){
         Route::get('/my',      'BookingController@getMyBookings');
         Route::post('/book',   'BookingController@createBooking');
-        Route::post('/all',    'BookingController@getAllBookings');
+        Route::get('/all',    'BookingController@getAllBookings');
         Route::get('/{id}',    'BookingController@getBooking')->where('id', '[0-9]+');
     });
 
